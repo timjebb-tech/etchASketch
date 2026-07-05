c7f11f074e53a59148a4cc915298a9b08ad05c16
Tim <275452338+timjebb-tech@users.noreply.github.com>
2026-07-05 11:53:54 +0100

    Implemented change number of squares

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,39 @@
 
-const containerSide = 4*200;
-const squareSide = containerSide / 4;
+const containerSide = 800;
+var squareCount = 16;
+var squareSide = containerSide / Math.sqrt(squareCount);
+
+var container = null;
+
+
+var addPromptButton = function(){
+  var button = document.createElement('button');
+  button.innerText = 'change number of squares';
+  button.onclick = changeNumberOfSquares;
+  // where do we want to have the button to appear?
+  // you can append it to another element just by doing something like
+  // document.getElementById('foobutton').appendChild(button);
+  document.body.appendChild(button);
+};
+
+function changeNumberOfSquares() {
+    var newNumber = parseInt(prompt("specify new number of squares.  I will round this up to a square number"));
+    console.log(`newNumber = ${newNumber}`);
+    var root = Math.sqrt(newNumber);
+    console.log(`root = ${root}`);
+    var ceil = Math.ceil(root);
+    console.log(`ceil = ${ceil}`);
+    squareCount = Math.pow(ceil, 2);
+    console.log(`squareCount = ${squareCount}`);
+    squareSide = containerSide / Math.sqrt(squareCount);
+    console.log(`squareSide = ${squareSide}`);
+    container.remove();
+    container=null;
+    container = createGrid();
+    document.body.appendChild(container);
+}
+
+
 
 function createSquare() {
     const square = document.createElement("div");
@@ -14,7 +47,7 @@ function createSquare() {
 }
 
 function createContainer() {
-    const container = document.createElement("div");
+    container = document.createElement("div");
     container.style.display="flex";
     container.style.flexFlow= "wrap";
     container.style.width=`${containerSide}px`;
@@ -24,7 +57,7 @@ function createContainer() {
 
 function createGrid() {
     container = createContainer();
-    for (var i = 0; i < 16; i++) {
+    for (var i = 0; i < squareCount; i++) {
         const square = createSquare();
         container.appendChild(square);
     }
@@ -35,6 +68,8 @@ function onHover(e) {
     e.style.background='blue';
 }
 
-var grid = createGrid();
+addPromptButton();
 
-document.body.appendChild(grid);
+container = createGrid();
+
+document.body.appendChild(container);
